f23b92ade19c94e6dc9ee205609af388cbd0c9b2
Samuel Monson <smonson@redhat.com>
2025-03-14 15:53:07 -0400

    Force legacy adapter to load module from src

--- load_test.py
+++ load_test.py
@@ -2,6 +2,13 @@
 
 """Legacy adapter for load_test"""
 
+from pathlib import Path
+import sys
+
+# HACK: Load llm_load_test from ./src
+src_dir = Path(__file__).parent / 'src'
+sys.path.insert(1, src_dir.as_posix())
+
 from llm_load_test.load_test import main
 
 if __name__ == "__main__":
